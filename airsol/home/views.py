dbc739f5719ae8dd840735cb9637f8cbcf6cb264
Aghahowa Jeffrey <aghahowaj01@gmail.com>
2024-04-15 08:53:41 -0700

    intital commit

---
+++ airsol/home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+# Create your views here.
