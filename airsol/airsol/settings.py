cf2e801bdca86a70230d656dfc44b844472584de
Aghahowa Jeffrey <aghahowaj01@gmail.com>
2024-04-21 19:00:02 -0700

    feat: add homepage

--- airsol/airsol/settings.py
+++ airsol/airsol/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a(w-8l(%5a0t+ii)%d%!c8$hmxc@+6_a6i12_6rr!y1l*rpvjc'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'airsol.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- airsol/home/views.py
+++ airsol/home/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from rest_framework import status
 
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def index(request):
+    """
+    Returns the home page and sends address to the backend.
+    """
+    if request.method == 'POST':
+        return None
+    
+    else: 
+        return render(request, 'index.html')
